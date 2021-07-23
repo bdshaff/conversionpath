97050e602ca9b64f9f0ef48e4b040ec0e2e13c8f
bdshaff <52110434+bdshaff@users.noreply.github.com>
2021-07-23 17:48:19 -0400

    Update plot_transition_matrix.R
    option to display absorbing states

--- R/plot_transition_matrix.R
+++ R/plot_transition_matrix.R
@@ -3,18 +3,29 @@
 #' @param transition_matrix transition_matrix
 #' @return plotly plot
 
-plot_transition_matrix <- function(transition_matrix) {
+plot_transition_matrix <- function(transition_matrix, full = FALSE) {
 
   if(!is.proper_transition_matrix(transition_matrix)){
     stop("Need a proper transition matrix")
   }
 
   M <- transition_matrix
-  plotly::plot_ly(
-    x = colnames(M[-c(1, 2, 3), -c(1, 2, 3)]),
-    y = row.names(M[-c(1, 2, 3), -c(1, 2, 3)]),
-    z = M[-c(1, 2, 3), -c(1, 2, 3)],
-    type = "heatmap",
-    colors = pals::brewer.divbin(3)
-  )
+
+  if(full){
+    plotly::plot_ly(
+      x = colnames(M),
+      y = row.names(M),
+      z = M,
+      type = "heatmap",
+      colors = pals::brewer.divbin(3)
+    )
+  }else{
+    plotly::plot_ly(
+      x = colnames(M[-c(1, 2, 3), -c(1, 2, 3)]),
+      y = row.names(M[-c(1, 2, 3), -c(1, 2, 3)]),
+      z = M[-c(1, 2, 3), -c(1, 2, 3)],
+      type = "heatmap",
+      colors = pals::brewer.divbin(3)
+    )
+  }
 }
