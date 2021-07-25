9c6d948d5178b0682882d1de70f8d3605c27b35d
bdshaff <52110434+bdshaff@users.noreply.github.com>
2021-07-25 17:02:42 -0400

    Update transition_matrix_from_markov_model.R
    remove %>%

--- R/transition_matrix_from_markov_model.R
+++ R/transition_matrix_from_markov_model.R
@@ -5,12 +5,11 @@
 #'
 transition_matrix_from_markov_model = function(mc_model){
 
-  trans_mat =
-    mc_model$transition_matrix %>%
-    pivot_wider(names_from = channel_to, values_from = transition_probability) %>%
-    relocate(`(null)`, .before = `1`) %>%
-    relocate(`(conversion)`, .before = `(null)`) %>%
-    mutate(start = 0, .before = `(conversion)`)
+  trans_mat = mc_model$transition_matrix
+  trans_mat <- tidyr::pivot_wider(trans_mat, names_from = channel_to, values_from = transition_probability)
+  trans_mat <- dplyr::relocate(trans_mat, `(null)`, .before = `1`)
+  trans_mat <- dplyr::relocate(`(conversion)`, .before = `(null)`)
+  trans_mat <- dplyr::mutate(start = 0, .before = `(conversion)`)
 
   trans_mat = as.matrix(trans_mat[,-1])
   trans_mat = rbind(
